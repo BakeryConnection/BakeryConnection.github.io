48af4a22f02b831af5de812f8bc3118a1fa1d3af
Fairyelf1818 <101997771+BakeryConnection@users.noreply.github.com>
2022-04-01 09:24:38 -0500

    The mail thingy was working fine here :)

--- js/mail.js
+++ js/mail.js
@@ -1,18 +1,22 @@
 var btn = document.getElementById('submit');
 btn.addEventListener('click',function(e){
     e.preventDefault()
-    var Name = document.getElementById('name').value;
-    var Email = document.getElementById('name').value;
-    var Phone = document.getElementById('phone').value;
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var messagebox = document.getElementById('ordertxt').value;
+    var phonenumber = document.getElementById('phone').value;
+    var deliverything = document.getElementById('deliveryway').value;
+    var thetext = name +" From: "+email+" and "+phonenumber+" says: "+ messagebox;
+    
     Email.send({
         Host : "smtp.gmail.com",
         Username : "bakeryconnection22@gmail.com",
         Password : "qhkmbnigkcnmevwm",
         To : 'bakeryconnection22@gmail.com',
         From : "you@isp.com",
-        Subject : Email,
-        Body : Name,
+        Subject : "Delivery Method: " +deliverything,
+        Body : thetext
     }).then(
-      message => alert("Order Placed :)")
+      message => alert("Your order has been placed <3")
     );
 })
