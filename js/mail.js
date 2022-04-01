4a8431da6e2f06d53a841b623f589028aa62d004
Fairyelf1818 <101997771+BakeryConnection@users.noreply.github.com>
2022-04-01 08:40:06 -0500

    working on ordering feature

--- js/mail.js
+++ js/mail.js
@@ -1,18 +1,18 @@
 var btn = document.getElementById('submit');
 btn.addEventListener('click',function(e){
     e.preventDefault()
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('name').value;
-    alert("hi")
+    var Name = document.getElementById('name').value;
+    var Email = document.getElementById('name').value;
+    var Phone = document.getElementById('phone').value;
     Email.send({
         Host : "smtp.gmail.com",
         Username : "bakeryconnection22@gmail.com",
         Password : "qhkmbnigkcnmevwm",
         To : 'bakeryconnection22@gmail.com',
         From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
+        Subject : Email,
+        Body : Name,
     }).then(
-      message => alert(message)
+      message => alert("Order Placed :)")
     );
 })
